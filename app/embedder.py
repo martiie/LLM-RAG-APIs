473c307442eff47bcd5f65d224da87de20a59593
martiie <640510702phithak@users.noreply.huggingface.co>
2025-06-18 09:38:57 +0000

    Update app/embedder.py

--- app/embedder.py
+++ app/embedder.py
@@ -1,10 +1,11 @@
 from sentence_transformers import SentenceTransformer
-from app.config import EMBED_MODEL_NAME
+# from app.config import EMBED_MODEL_NAME
 import os
-os.environ['TRANSFORMERS_CACHE'] = '/tmp/transformers'
+os.environ['TRANSFORMERS_CACHE'] = '/tmp/transformers_cache'
+os.environ['HF_HOME'] = '/tmp/huggingface'
 os.environ['SENTENCE_TRANSFORMERS_HOME'] = '/tmp/sentence_transformers'
 
-embed_model = SentenceTransformer(EMBED_MODEL_NAME)
+embed_model = SentenceTransformer("all-MiniLM-L6-v2")
 
 def embed_texts(texts):
     return embed_model.encode(texts, convert_to_numpy=True)
