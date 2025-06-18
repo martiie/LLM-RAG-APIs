3d5e8c820a0ca84689f18e49b1fed25e1f8c537c
martiie <640510702phithak@users.noreply.huggingface.co>
2025-06-18 10:03:26 +0000

    Update app/config.py

--- app/config.py
+++ app/config.py
@@ -4,6 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 EMBED_MODEL_NAME = 'all-MiniLM-L6-v2'
-DOCS_JSON = 'docs.json'
+DOCS_JSON = "/tmp/docs.json"
 GENAI_MODEL = 'gemini-2.0-flash'
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
